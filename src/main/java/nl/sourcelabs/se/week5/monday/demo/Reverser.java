c0019a34e5646a627b8e27e73fc23425ff3e3d16
Jarno Walgemoed <j.walgemoed@sourcelabs.nl>
2017-10-09 11:46:35 +0200

    fixed pom

--- src/main/java/nl/sourcelabs/se/week5/monday/demo/Reverser.java
+++ src/main/java/nl/sourcelabs/se/week5/monday/demo/Reverser.java
@@ -1,7 +1,7 @@
 package nl.sourcelabs.se.week5.monday.demo;
 
 /**
- * Assignment: Test this class. Define test cases and test if everything works as expected.
+ * Define test cases and test if everything works as expected.
  * 
  * Reverser class, for some reversing awesomeness
  */
@@ -12,12 +12,17 @@ public class Reverser {
      *
      * @param source
      *         the string to reverse
-     *         
+     *         hello
      * @return the source string reversed
+     * @throws IllegalArgumentException when source null is null
      */
     public String reverse(final String source) {
+        if(source == null) {
+            throw new IllegalArgumentException("Source cannot be null");
+        }
+        
         final StringBuilder result = new StringBuilder();
-        for (int i = source.length(); i > 0; i--) {
+        for (int i = source.length() - 1; i >= 0; i--) {
             result.append(source.charAt(i));
         }
         return result.toString();
--- src/test/java/nl/sourcelabs/se/week5/monday/demo/ReverserTest.java
+++ src/test/java/nl/sourcelabs/se/week5/monday/demo/ReverserTest.java
@@ -1,5 +1,29 @@
 package nl.sourcelabs.se.week5.monday.demo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class ReverserTest {
     
+    @Test
+    public void testReverseHallo() {
+        assertEquals("ollah", new Reverser().reverse("hallo"));
+    }
+    
+    @Test
+    public void testReverseHallO() {
+        assertEquals("OllaH", new Reverser().reverse("HallO"));
+    }
+    
+    @Test
+    public void testEmptyString() {
+        assertEquals("", new Reverser().reverse(""));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullValue() { 
+        assertNull(new Reverser().reverse(null)); 
+    }
 }
